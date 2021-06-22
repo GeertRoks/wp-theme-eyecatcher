18ff5de65dba3627149a38819ad2e2f2d4c37653
Geert Roks <31696350+GeertRoks@users.noreply.github.com>
2021-06-22 15:46:07 +0200

    Initial commit - Underscores.me template
    This wordpress theme is started with the template from underscores.me.
    The theme is called eyecatcher, because it will catch all the eyes ;)

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Eyecatcher
+ */
+
+?>
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'eyecatcher' ) ); ?>">
+				<?php
+				/* translators: %s: CMS name, i.e. WordPress. */
+				printf( esc_html__( 'Proudly powered by %s', 'eyecatcher' ), 'WordPress' );
+				?>
+			</a>
+			<span class="sep"> | </span>
+				<?php
+				/* translators: 1: Theme name, 2: Theme author. */
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'eyecatcher' ), 'eyecatcher', '<a href="http://www.github.com/GeertRoks">Geert Roks</a>' );
+				?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
